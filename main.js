eb33e74ad98e069aa2be1fc401f819b6f6592b2e
gamaoutlet1-pixel <gamaoutlet1@gmail.com>
2026-09-16 17:07:23 -0300

    Adiciona entrada do aplicativo Windows

---
+++ main.js
@@ -0,0 +1,5 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+function createWindow(){const win=new BrowserWindow({width:1440,height:900,minWidth:1100,minHeight:700,backgroundColor:'#f4f5f7',autoHideMenuBar:true,webPreferences:{preload:path.join(__dirname,'preload.js'),contextIsolation:true,nodeIntegration:false}});win.loadFile('index.html');}
+app.whenReady().then(()=>{createWindow();app.on('activate',()=>{if(BrowserWindow.getAllWindows().length===0)createWindow();});});
+app.on('window-all-closed',()=>{if(process.platform!=='darwin')app.quit();});
